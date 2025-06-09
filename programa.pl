98ad4987f77f3995a7c24cb26c96d57241a2ff35
asanzo <alfredo.sanzo@gmail.com>
2025-06-09 02:05:36 -0300

    Subo base de conocimientos

--- programa.pl
+++ programa.pl
@@ -1,1 +1,68 @@
-% Aquí va el código.
+estaEn(americaDelSur, argentina).
+estaEn(americaDelSur, brasil).
+estaEn(americaDelSur, chile).
+estaEn(americaDelSur, uruguay).
+estaEn(americaDelNorte, alaska).
+estaEn(americaDelNorte, yukon).
+estaEn(americaDelNorte, canada).
+estaEn(americaDelNorte, oregon).
+estaEn(asia, kamtchatka).
+estaEn(asia, china).
+estaEn(asia, siberia).
+estaEn(asia, japon).
+estaEn(oceania,australia).
+estaEn(oceania,sumatra).
+estaEn(oceania,java).
+estaEn(oceania,borneo).
+
+limitrofes(argentina,brasil).
+limitrofes(argentina,chile).
+limitrofes(argentina,uruguay).
+limitrofes(uruguay,brasil).
+limitrofes(alaska,kamtchatka).
+limitrofes(alaska,yukon).
+limitrofes(canada,yukon).
+limitrofes(alaska,oregon).
+limitrofes(canada,oregon).
+limitrofes(siberia,kamtchatka).
+limitrofes(siberia,china).
+limitrofes(china,kamtchatka).
+limitrofes(japon,china).
+limitrofes(japon,kamtchatka).
+limitrofes(australia,sumatra).
+limitrofes(australia,java).
+limitrofes(australia,borneo).
+limitrofes(australia,chile).
+
+% Usar este para saber si son limítrofes ya que es una relación simétrica
+sonLimitrofes(X, Y) :- limitrofes(X, Y).
+sonLimitrofes(X, Y) :- limitrofes(Y, X).
+
+jugador(amarillo).
+jugador(magenta).
+jugador(negro).
+jugador(blanco).
+
+alianza(amarillo,magenta).
+
+%el numero son los ejercitos
+ocupa(argentina, magenta, 5).
+ocupa(chile, negro, 3).
+ocupa(brasil, amarillo, 8).
+ocupa(uruguay, magenta, 5).
+ocupa(alaska, amarillo, 7).
+ocupa(yukon, amarillo, 1).
+ocupa(canada, amarillo, 10).
+ocupa(oregon, amarillo, 5).
+ocupa(kamtchatka, negro, 6).
+ocupa(china, amarillo, 2).
+ocupa(siberia, amarillo, 5).
+ocupa(japon, amarillo, 7).
+ocupa(australia, negro, 8).
+ocupa(sumatra, negro, 3).
+ocupa(java, negro, 4).
+ocupa(borneo, negro, 1).
+
+
+
+
