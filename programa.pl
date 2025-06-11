c00ebd56d166c2fafa2a547fd47e0e8423596b1a
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2025-06-10 22:10:18 -0300

    hecho en clase

--- programa.pl
+++ programa.pl
@@ -1,3 +1,59 @@
+% Resolviendo vomistar
+
+departamento(maria, ventas).
+departamento(juan, ventas).
+departamento(roque, ventas).
+departamento(nora, compras).
+departamento(pedro, compras).
+departamento(felipe, administracion).
+departamento(hugo, administracion).
+departamento(ana, administracion).
+
+puesto(maria, empleado).
+puesto(nora, empleado).
+puesto(felipe, empleado).
+puesto(hugo, empleado).
+puesto(juan, cadete).
+puesto(pedro, cadete).
+puesto(ana, cadete).
+puesto(roque, pasante).
+
+% Es un predicado completamente inversible (porque puedo hacer consultas existenciales con ambas variables)
+% Es una relación simétrica (porque si se cumple para A y B entonces también para B y A)
+% Es una relación reflexiva (porque se cumple para A y A) 
+% Es una relación transitiva (ya saben por qué)
+trabajanMismoDepto(Persona1, Persona2):-
+    departamento(Persona1, Depto),
+    departamento(Persona2, Depto).
+
+% puedeDarleOrdenes(maria,pedro). -> No, que deduzca el programa
+
+puedeDarleOrdenes(Jefe, Subordinado):-
+    puesto(Jefe, PuestoJefe),
+    puesto(Subordinado, PuestoSubordinado),
+    superiorA(PuestoJefe,PuestoSubordinado),
+    trabajanMismoDepto(Subordinado, Jefe).
+
+superiorA(empleado, cadete).
+superiorA(empleado, pasante).
+
+seLlevanBien(Persona1, Persona2) :-
+    trabajanMismoDepto(Persona1,Persona2),
+    not(puedeDarleOrdenes(Persona1,Persona2)),
+    not(puedeDarleOrdenes(Persona2,Persona1)).
+
+%-begin_tests(puedeDarleOrdenes).
+% Son 3 clases de equivalencia. El nombre de la clase de equivalencia es el nombre del test:
+test(mismoDeptoYEsPuestoSuperiorPuedeDarOrdenes):-
+    puedeDarleOrdenes(maria,nora). % representan la clase de equivalencia
+test(mismoDeptoYEsPuestoNoEsSuperiorNoPuedeDarOrdenes):-
+    puedeDarleOrdenes(nora, maria).
+test(distintoDeptoAunqueSeaSuperiorNoPuedeDarOrdenes):-
+    puedeDarleOrdenes(hugo,juan).
+%:-end_tests(puedeDarleOrdenes).
+
+
+
 estaEn(americaDelSur, argentina).
 estaEn(americaDelSur, brasil).
 estaEn(americaDelSur, chile).
@@ -63,6 +119,32 @@ ocupa(sumatra, negro, 3).
 ocupa(java, negro, 4).
 ocupa(borneo, negro, 1).
 
+puedenAtacarse(Jugador1, Jugador2):-
+    ocupa(Pais1, Jugador1, _),
+    ocupa(Pais2, Jugador2, _),
+    sonLimitrofes(Pais1, Pais2).
 
+estaTodoBien(Jugador1, Jugador2):-
+    jugador(Jugador1),
+    jugador(Jugador2),
+    not(puedenAtacarse(Jugador1, Jugador2)).
+
+estaTodoBien(Jugador1, Jugador2):-
+    sonAliados(Jugador1, Jugador2).
+
+sonAliados(Jugador1, Jugador2):- alianza(Jugador1, Jugador2).
+sonAliados(Jugador1, Jugador2):- alianza(Jugador2, Jugador1).
+
+ocupaContinente(Jugador, Continente):-
+    jugador(Jugador),
+    continente(Continente),
+    forall(estaEn(Continente, Pais), ocupa(Pais, Jugador, _)).
+
+continente(Continente):-
+    estaEn(Continente, _).
+
+elQueTieneMasEjercitos(Jugador):-
+    ocupa(_, Jugador, NumeroGroso),
+    forall(ocupa(_,_,CantEjercitos), NumeroGroso >= CantEjercitos).
 
 
